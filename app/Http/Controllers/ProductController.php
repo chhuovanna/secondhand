19b566c91a7729f54cb31832877a2405e28185ee
lamthitha <lamthitha.itc@gmail.com>
2019-07-31 14:47:16 +0700

    update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,11 +17,13 @@ use DB;
 class ProductController extends Controller
 {
     protected $productwithpost;
+
     public function index()
     {
 
         return view('category.productindex');
     }
+
     public function create()
     {
         $categories = Category::getSelectOptions();
@@ -31,13 +33,14 @@ class ProductController extends Controller
     public function createwitholdpost($post_id)
     {
         $categories = Category::getSelectOptions();
-        $product = Product::where('post_id','=', $post_id)->first();
+        $product = Product::where('post_id', '=', $post_id)->first();
 
-       return view('category.productcreate', ['categories' => $categories
-                                            , 'post_id' =>$post_id
-                                            ,'pickup_time'=>$product->pickup_time
-                                            ,'pickup_address'=>$product->pickup_address]);
+        return view('category.productcreate', ['categories' => $categories
+            , 'post_id' => $post_id
+            , 'pickup_time' => $product->pickup_time
+            , 'pickup_address' => $product->pickup_address]);
     }
+
     public function store(Request $request)
     {
         $product = new Product();
@@ -59,21 +62,21 @@ class ProductController extends Controller
         //get file from input
         $file = $request->file('thumbnail_id');
         $thumbnail = new Image();
-        $thumbnail->file_name = rand(1111,9999).time().'.'.$file->getClientOriginalExtension();
+        $thumbnail->file_name = rand(1111, 9999) . time() . '.' . $file->getClientOriginalExtension();
         $thumbnail->location = 'images\thumbnail'; //thumbnail is stored in public/images/thumbnail
 
         try {
             $thumbnail->save();
             //product the file to it's location on server
-            $file->move(public_path($thumbnail->location),$thumbnail->file_name);
+            $file->move(public_path($thumbnail->location), $thumbnail->file_name);
 
             //thumbnail of product
             $product->image_id = $thumbnail->image_id;
-            if ($request->get('post_id') !== null){
+            if ($request->get('post_id') !== null) {
                 $product->post_id = $request->get('post_id');
-            }else{
+            } else {
                 $user_id = Auth::id();
-                $seller = Seller::where('user_id','=',$user_id)->first();
+                $seller = Seller::where('user_id', '=', $user_id)->first();
                 $post = new Post();
                 $post->seller_id = $seller->seller_id;
                 $post->save();
@@ -84,49 +87,51 @@ class ProductController extends Controller
 
 
             //test if user has upload other photos or not
-            if($request->hasFile('photos')){
+            if ($request->hasFile('photos')) {
                 //get the array of photos
                 $photos = $request->file('photos');
                 foreach ($photos as $key => $file) {
                     $photo = new Image();
-                    $photo->file_name = rand(1111,9999).time().'.'.$file->getClientOriginalExtension();
+                    $photo->file_name = rand(1111, 9999) . time() . '.' . $file->getClientOriginalExtension();
                     //photos are stored on server in folder public/images/photos
                     $photo->location = 'images\photos';
 
                     //photo belongs to product
                     $photo->product_id = $product->product_id; //not (id) product_id
                     $photo->save();
-                    $file->move(public_path($photo->location),$photo->file_name);
+                    $file->move(public_path($photo->location), $photo->file_name);
                 }
             }
             $category = $request->get('category_id');
-            if(sizeof($category)>0){
+            if (sizeof($category) > 0) {
                 $product->category()->attach($category);
             }
-
 
 
             //dd($category);
 
 
-            if($request->get('add_more') == 1){
+            if ($request->get('add_more') == 1) {
 
 
-                return redirect()->route('product.create.with.oldpost',['post_id'=>$product->post_id])->withFlashSuccess('Product is added. You wish you add more product in the same post. We suggest the same pickup time and date');
+                return redirect()->route('product.create.with.oldpost', ['post_id' => $product->post_id])->withFlashSuccess('Product is added. You wish you add more product in the same post. We suggest the same pickup time and date');
             }
             return redirect()->route('product.index')->withFlashSuccess('Product is added');
-        }
-        catch (\Exception $e) {
+        } catch (\Exception $e) {
             return redirect()
-            ->back()
-            ->withInput($request->all())
-            ->withFlashDanger("Product can't be added. ". $e->getMessage());
+                ->back()
+                ->withInput($request->all())
+                ->withFlashDanger("Product can't be added. " . $e->getMessage());
         }
     }
-    public function show($id) {
-        echo 'showlalal'.$id;
+
+    public function show($id)
+    {
+        echo 'showlalal' . $id;
     }
-    public function edit($id) {
+
+    public function edit($id)
+    {
 
         $product = Product::with('thumbnail')->with('photo')->with('post')->find($id);
         $post = $product->post;
@@ -135,148 +140,161 @@ class ProductController extends Controller
         $categories = Category::getSelectOptions();
 
 
-
         if (Auth::user()->hasRole('administrator')) {
             return view('category.productedit', ['categories' => $categories, 'product' => $product]);
-        }elseif ($seller->user_id == Auth::id()){
+        } elseif ($seller->user_id == Auth::id()) {
             return view('category.productedit', ['categories' => $categories, 'product' => $product]);
-        }else{
+        } else {
             return redirect()->back()->withFlashDanger("You don't have the permission");
         }
 
     }
-    public function update(Request $request, $id) {
+
+    public function update(Request $request, $id)
+    {
         $product = Product::with('thumbnail')->with('photo')->with('post')->find($id);
         $post = $product->post;
         $seller = Seller::find($post->seller_id);
+        $permit = true;
+        if (Auth::user()->hasRole('administrator')) {
+            $permit = true;
+        } else {
+            $user = Auth::id();
+            $seller = Seller::where('user_id', $user)->first();
+            if ($seller->seller_id == $id) {
+                $permit = true;
+            } else {
+                return redirect()
+                    ->back()
+                    ->withFlashDanger("You dont have the permission ");
+            }
+        }
+        if ($permit) {
+            $product = Product::find($id);
+            $product->product_id = $request->get('product_id');
+            $product->name = $request->get('name');
+            $product->price = $request->get('price');
+            $product->description = $request->get('description');
+            //$product->view_number = $request->get('view_number');
+            $product->status = $request->get('status');
+            $product->pickup_address = $request->get('pickup_address');
+            $product->pickup_time = $request->get('pickup_time');
+            //$product->created_at = $request->get('created_at');
+            //$product->updated_at = $request->get('updated_at');
+            //$product->post_id = $request->get('post_id');
+            //$product->image_id = $request->get('image_id');
 
-        $product->product_id = $request->get('product_id');
-        $product->name = $request->get('name');
-        $product->price = $request->get('price');
-        $product->description = $request->get('description');
-        //$product->view_number = $request->get('view_number');
-        $product->status = $request->get('status');
-        $product->pickup_address = $request->get('pickup_address');
-        $product->pickup_time = $request->get('pickup_time');
-        //$product->created_at = $request->get('created_at');
-        //$product->updated_at = $request->get('updated_at');
-        //$product->post_id = $request->get('post_id');
-        //$product->image_id = $request->get('image_id');
-
-        $validateData = $request->validate([
-			'thumbnail_id' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
-			,'photos[]' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048']);
-
-
-        try{
-			// test if thumbnail is updated or not
-			if($request->hasFile('thumbnail_id')){
-				$file = $request->file('thumbnail_id');
-				$thumbnail = new Image();
-				$thumbnail->file_name = rand(1111,9999).time().'.'.$file->getClientOriginalExtension();
-				$thumbnail->location = 'images\thumbnail';
-
-				$file->move(public_path($thumbnail->location),$thumbnail->file_name);
-				$thumbnail->save();//save new thumbnail
-
-
-				$old_thumbnail = $product->thumbnail; // Keep the old thumbnail for removing if it exists
-				$product->image_id = $thumbnail->image_id;	//change the thumbnail to the new one
-
-
-			}
+            $validateData = $request->validate([
+                'thumbnail_id' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+                , 'photos[]' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048']);
 
 
+            try {
+                // test if thumbnail is updated or not
+                if ($request->hasFile('thumbnail_id')) {
+                    $file = $request->file('thumbnail_id');
+                    $thumbnail = new Image();
+                    $thumbnail->file_name = rand(1111, 9999) . time() . '.' . $file->getClientOriginalExtension();
+                    $thumbnail->location = 'images\thumbnail';
 
-			$product->save(); //save the update of product
-
-			if(isset($old_thumbnail)){
-				//remove old thumbnail from harddisk
-				$file = public_path($old_thumbnail->location).'\\'.$old_thumbnail->file_name;
-				if ( File::exists($file)) {
-					File::delete($file);
-				}
-
-				$old_thumbnail->delete(); //delete the old thumbnail if user add a new one
-			}
+                    $file->move(public_path($thumbnail->location), $thumbnail->file_name);
+                    $thumbnail->save();//save new thumbnail
 
 
-
-			$db_old_photos = $product->photo;//get old photos from db
-            if($db_old_photos){// if there is any old photos in db
-
-				$old_photos = $request->get('old_photos'); //get the list of old photos after use update
-
-    //            print_r($old_photos);
-				foreach($db_old_photos as $db_old_photo){
-
-					//test if user has deleted all old photos, we remove it from db and hard disk
-					//or test if some old photos are deleted by user, we remove it form db and hard disk
-					if (!$old_photos or ($old_photos && !in_array($db_old_photo->image_id, $old_photos))){
-
-						if($db_old_photo->delete()){
-							//remove old thumbnail from harddisk
-							$file = public_path($db_old_photo->location).'\\'.$db_old_photo->file_name;
-							if ( File::exists($file)) {
-								File::delete($file);
-							}
-						}
-					}
-				}
-			}
+                    $old_thumbnail = $product->thumbnail; // Keep the old thumbnail for removing if it exists
+                    $product->image_id = $thumbnail->image_id;    //change the thumbnail to the new one
 
 
+                }
 
 
-			//test if user has upload other photos or not
-			if($request->hasFile('photos')){
+                $product->save(); //save the update of product
+
+                if (isset($old_thumbnail)) {
+                    //remove old thumbnail from harddisk
+                    $file = public_path($old_thumbnail->location) . '\\' . $old_thumbnail->file_name;
+                    if (File::exists($file)) {
+                        File::delete($file);
+                    }
+
+                    $old_thumbnail->delete(); //delete the old thumbnail if user add a new one
+                }
 
 
+                $db_old_photos = $product->photo;//get old photos from db
+                if ($db_old_photos) {// if there is any old photos in db
 
-				//get the array of photos
-				$photos = $request->file('photos');
+                    $old_photos = $request->get('old_photos'); //get the list of old photos after use update
+
+                    //            print_r($old_photos);
+                    foreach ($db_old_photos as $db_old_photo) {
+
+                        //test if user has deleted all old photos, we remove it from db and hard disk
+                        //or test if some old photos are deleted by user, we remove it form db and hard disk
+                        if (!$old_photos or ($old_photos && !in_array($db_old_photo->image_id, $old_photos))) {
+
+                            if ($db_old_photo->delete()) {
+                                //remove old thumbnail from harddisk
+                                $file = public_path($db_old_photo->location) . '\\' . $db_old_photo->file_name;
+                                if (File::exists($file)) {
+                                    File::delete($file);
+                                }
+                            }
+                        }
+                    }
+                }
 
 
-
-				foreach ($photos as $file) {
-					$photo = new Image();
-					$photo->file_name = rand(1111,9999).time().'.'.$file->getClientOriginalExtension();
-
-					//photos are stored on server in folder public/images/photos
-					$photo->location = 'images\photos';
-
-					//photo belongs to product
-					$photo->product_id = $request->get('product_id');
-					$photo->save();
-					$file->move(public_path($photo->location),$photo->file_name);
+                //test if user has upload other photos or not
+                if ($request->hasFile('photos')) {
 
 
-				}
-			}
+                    //get the array of photos
+                    $photos = $request->file('photos');
 
 
+                    foreach ($photos as $file) {
+                        $photo = new Image();
+                        $photo->file_name = rand(1111, 9999) . time() . '.' . $file->getClientOriginalExtension();
 
-			return redirect()->route('product.index')->withFlashSuccess('product is updated');
-        }catch(\Exception $e){
-            return redirect()
-                ->back()
-                ->withInput($request->all())
-                ->withFlashDanger("Product can't be updated. ". $e->getMessage());
+                        //photos are stored on server in folder public/images/photos
+                        $photo->location = 'images\photos';
+
+                        //photo belongs to product
+                        $photo->product_id = $request->get('product_id');
+                        $photo->save();
+                        $file->move(public_path($photo->location), $photo->file_name);
+
+
+                    }
+                }
+
+
+                return redirect()->route('product.index')->withFlashSuccess('product is updated');
+            } catch (\Exception $e) {
+                return redirect()
+                    ->back()
+                    ->withInput($request->all())
+                    ->withFlashDanger("Product can't be updated. " . $e->getMessage());
+            }
         }
     }
-    public function destroy($id) {
-        try{
+
+    public function destroy($id){
+        if (Auth::user()->hasRole('administrator')) {
+
+        try {
 
             //to get the array of photos of the product
             $product = Product::with('photo')->with('thumbnail')->find($id);
             $photos = $product->photo;
             $res['photos'] = true;
-            if($photos){
+            if ($photos) {
                 foreach ($photos as $photo) {
-                    $file = public_path($photo->location).'\\'.$photo->file_name;
-                    if ( File::exists($file)) {
+                    $file = public_path($photo->location) . '\\' . $photo->file_name;
+                    if (File::exists($file)) {
 
-                        if(File::delete($file)){//delete the file from the folder
+                        if (File::delete($file)) {//delete the file from the folder
                             $res['photos'] = $res['photos'] && $photo->delete(); //delete the file from database
                         }
 
@@ -284,8 +302,6 @@ class ProductController extends Controller
                 }
 
             }
-
-
 
 
             //to get thumbnail of the product to be deleted. in product model, there is function called thumbnail
@@ -297,16 +313,16 @@ class ProductController extends Controller
             $res['product'] = Product::destroy($id);
 
             $otherproduct = $post->product;
-            if(sizeof($otherproduct) == 0){
+            if (sizeof($otherproduct) == 0) {
                 $post->delete();
             }
 
-            if ($thumbnail){
-                $file = $file = public_path($thumbnail->location).'\\'.$thumbnail->file_name;
+            if ($thumbnail) {
+                $file = $file = public_path($thumbnail->location) . '\\' . $thumbnail->file_name;
                 //test if the thumbnail file exists or not
-                if ( File::exists($file)) {
+                if (File::exists($file)) {
                     //delete the file from the folder
-                    if(File::delete($file)){
+                    if (File::delete($file)) {
                         //delete the thumbnail of the product from database;
                         $res['thumbnail'] = $thumbnail->delete();
                     }
@@ -314,19 +330,25 @@ class ProductController extends Controller
             }
 
 
-            if ($res['product'] )
+            if ($res['product'])
                 return [1];
             else
                 return [0];
-        }catch(\Exception $e){
-            return [0,$e->getMessage()];
+        } catch (\Exception $e) {
+            return [0, $e->getMessage()];
         }
-    }
-    public function getform(){
-        $products = product::all();
-        //$reviewers = reviewer::all();//???
-        return view('productrate', [ 'products' => $products  ]);
-    }
+    }else{
+            return [0, "You don't have the permission"];
+        }
+}
+
+
+
+//    public function getform(){
+//        $products = product::all();
+//        //$reviewers = reviewer::all();//???
+//        return view('productrate', [ 'products' => $products  ]);
+//    }
     // public function saverating(Request $request){
 
     //     $rating = new Rating();
@@ -393,8 +415,6 @@ class ProductController extends Controller
     public function getproduct(){
 
 
-
-        
         if(Auth::user()->hasRole('administrator')){ //is admin, but need to modify
             $products = Product::select(['product.product_id', 'product.name'/*DB::raw('product.name as pname')*/, 'price'
                                     , 'description','view_number','status','pickup_address','pickup_time','created_at'
