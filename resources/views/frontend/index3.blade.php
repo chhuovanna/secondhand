1bc6ab7c1dc5052b8c76de33807d6ed5506a7fb4
lamthitha <lamthitha.itc@gmail.com>
2019-08-05 17:34:09 +0700

    update

--- resources/views/frontend/index3.blade.php
+++ resources/views/frontend/index3.blade.php
@@ -585,7 +585,7 @@
 						</button>
 
 						<input class="mtext-107 cl2 size-114 plh2 p-r-15" type="text" name="search-product" placeholder="Search">
-						
+
 					</div>
 				</div>
 
@@ -797,7 +797,7 @@
 			<div class="row isotope-grid">
 				@foreach ($products as $product)
 					@php /*$director = str_replace(' ','-',$product->director);*/$director='test';@endphp
-				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item {{$director}}" data-Product_id="{{$product->product_id}}">
+				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item {{$director}}" data-mid="{{$product->product_id}}">
 					<!-- Block2 -->
 					<div class="block2">
 						<div class="block2-pic hov-img0">
@@ -806,7 +806,7 @@
 							@else
 							<img src="{{asset('images/thumbnail')}}/default.png" alt="IMG-PRODUCT">
 							@endif
-							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1" data-product_id="{{$product->product_id}}">
+							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1" data-mid="{{$product->product_id}}">
 								Quick View
 							</a>
 						</div>
